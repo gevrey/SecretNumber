85ea3b6e8505ab2e59a585b8a3eb0d0ebe94b37f
Yves Feigneux <yves.feigneux@gmail.com>
2013-07-12 16:27:21 +0200

    added verified guess

--- Resources/businessRules.js
+++ Resources/businessRules.js
@@ -7,8 +7,15 @@ function getRandomNumber(maxNumber) {
 	return Math.floor(Math.random( ) * (maxNumber+1));
 }
 
-/*
-function verifyGuess(guessedNumber, maxNumber) {
+
+function verifyGuess(guessedNumber, secretNumber) {
+	var result = 0;
+	if (guessedNumber < secretNumber) {
+		result=-1;
+	}
+	else if (guessedNumber > secretNumber) {
+		result=1;
+	}
+	return result;
 
 }
-*/
